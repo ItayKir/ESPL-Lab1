58284e23c0709fc680ab26791704fb3bfd8502cf
Itay Kigner <itaykirgner@gmail.com>
2026-04-13 17:57:55 +0000

    Finished part 2

--- base.c
+++ base.c
@@ -16,6 +16,46 @@ char replaceChar(char c){
     return c;
 }
 
+char my_get(char c){
+    return (char)fgetc(stdin);
+}
+
+char cxprt(char c){
+    if(c >= 0x20 && c <= 0x7E){
+        printf("%c", c);
+    }
+    else{
+        printf(".");
+    }
+    printf(" %x\n", c);
+    return c;
+}
+
+char encrypt(char c){
+    if(c == 0x7F){
+        return 0x20;
+    }
+    if(c>= 0x20 && c < 0x7F){
+        return c+1;
+    }
+    return c;
+}
+
+char decrypt(char c){
+    if(c == 0x20){
+        return 0x7F;
+    }
+    if(c> 0x20 && c <= 0x7F){
+        return c-1;
+    }
+    return c;    
+}
+
+char dprt(char c){
+    printf("%d\n", c);
+    return c;
+}
+
 char* map(char *array, int array_length, char (*f) (char)){
     char* mapped_array = (char*)(malloc(array_length*sizeof(char)));
     /* TODO: Complete during task 2.a */
@@ -27,8 +67,14 @@ char* map(char *array, int array_length, char (*f) (char)){
  
 int main(int argc, char **argv){
   /* TODO: Test your code */
-    char arr1[] = {'H','E','Y','!'};
-    char* arr2 = map(arr1, 4, replaceChar);
-    printf("%s\n", arr2);
+    int base_len = 5;
+    char arr1[base_len];
+    char* arr2 = map(arr1, base_len, my_get);
+    char* arr3 = map(arr2, base_len, dprt);
+    char* arr4 = map(arr3, base_len, cxprt);
     free(arr2);
+    free(arr3);
+    free(arr4);
+
+    return 0;
 }
